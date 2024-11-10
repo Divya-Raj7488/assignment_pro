2716f22c1ce21b0a1a6d593245e07d83411f0df8
Divya Raj <divyaraj7612@gmail.com>
2024-11-11 04:23:49 +0530

    create navbar

--- frontend/src/app/components/nav.jsx
+++ frontend/src/app/components/nav.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import { CiMenuKebab } from "react-icons/ci";
+import "../styles/nav.css";
+import Logo from "../assets/Logo.svg";
+import Image from "next/image";
+import { BsSearch } from "react-icons/bs";
 
-const Nav = () => {
-  return <div></div>;
-};
-
-export default Nav;
+export default function Nav() {
+  const [searchParam, setSearchParam] = useState("");
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <Image src={Logo} alt="Logo" style={{ width: "90%", height: "90%" }} />
+      </div>
+      <div className="links">
+        <ul>
+          <li>Music</li>
+          <li>Podcasts</li>
+          <li>Live</li>
+          <li>Radio</li>
+        </ul>
+        <div className="searchBar">
+          <input
+            type="search"
+            value={searchParam}
+            onChange={(e) => {
+              setSearchParam(e.target.value);
+            }}
+            className="searchTag"
+          />
+          <BsSearch style={{ width: "26px", height: "26px", color: "white", marginLeft:"-2.5rem" }} />
+        </div>
+      </div>
+      <div className="hamburgerIcon">
+        <CiMenuKebab className="threeDots" />
+      </div>
+    </div>
+  );
+}
